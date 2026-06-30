40a46576e3e6f197995a209d8ed15c0cad15391a
nimeshwsth-alt <nimeshwsth@gmail.com>
2026-06-30 20:50:45 +0530

    Refactor load_data.py by removing comments
    Removed comments and added a newline at the end of the file.

--- src/load_data.py
+++ src/load_data.py
@@ -1,24 +1,17 @@
 import pandas as pd
-
-# Load CSV file
 df = pd.read_csv("data/student_dataset_v2.csv")
 
-# Display first 5 rows
 print("FIRST 5 RECORDS")
 print(df.head())
 
-# Display last 5 rows
 print("\nLAST 5 RECORDS")
 print(df.tail())
 
-# Shape
 print("\nDATASET SHAPE")
 print(df.shape)
 
-# Column names
 print("\nCOLUMN NAMES")
 print(df.columns)
 
-# Data types
 print("\nDATA TYPES")
 print(df.dtypes)
