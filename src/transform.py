0c1fdaf604cf57505be3cf4af357d65f2463c354
nimeshwsth-alt <nimeshwsth@gmail.com>
2026-06-30 20:51:52 +0530

    Refactor grade calculation and data saving process

--- src/transform.py
+++ src/transform.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-# Load cleaned data
 df = pd.read_csv("output/cleaned_data.csv")
 
-# Create grade column
 def get_grade(marks):
     if marks >= 90:
         return "A"
@@ -18,7 +16,6 @@ def get_grade(marks):
 
 df["Grade"] = df["Marks"].apply(get_grade)
 
-# Save transformed data
 df.to_csv("output/transformed_data.csv", index=False)
 
 print("Transformation completed!")
